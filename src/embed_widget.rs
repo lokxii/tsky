c99248bf639c303fd022f609b292fa45be1a0c67
lokxii <9tin9tin9@gmail.com>
2024-10-22 17:05:26 +0800

    Refactoring: embed widget -> module

---
+++ src/embed_widget.rs
@@ -0,0 +1,84 @@
+use ratatui::{
+    style::{Color, Modifier, Style},
+    symbols,
+    text::Line,
+    widgets::{Block, Paragraph, Widget},
+};
+
+use crate::{embed::Embed, RecordWidget};
+
+pub struct EmbedWidget {
+    pub embed: Embed,
+    pub style: Style,
+    pub is_selected: bool,
+}
+
+impl EmbedWidget {
+    pub fn new(embed: Embed, is_selected: bool) -> EmbedWidget {
+        EmbedWidget {
+            embed,
+            style: if is_selected {
+                Style::default().bg(Color::Rgb(45, 50, 55))
+            } else {
+                Style::default()
+            },
+            is_selected,
+        }
+    }
+
+    pub fn line_count(&self, width: u16) -> u16 {
+        if let Embed::Record(record) = &self.embed {
+            RecordWidget::new(record.clone(), false).line_count(width) as u16
+        } else {
+            self.non_record_paragraph().line_count(width - 2) as u16 + 2
+        }
+    }
+
+    fn non_record_paragraph(&self) -> Paragraph {
+        match &self.embed {
+            Embed::Images(images) => Paragraph::new(
+                images
+                    .iter()
+                    .map(|image| {
+                        Line::from(format!("[image, alt: {}]", image.alt))
+                    })
+                    .collect::<Vec<Line>>(),
+            ),
+
+            Embed::Video(video) => {
+                Paragraph::new(format!("[video, alt: {}]", video.alt))
+            }
+
+            Embed::External(external) => Paragraph::new(vec![
+                Line::from(external.title.clone())
+                    .style(Style::default().add_modifier(Modifier::BOLD)),
+                Line::from(external.description.clone()),
+                Line::from(external.url.clone())
+                    .style(Style::default().add_modifier(Modifier::UNDERLINED)),
+            ]),
+
+            Embed::Record(_) => panic!("Shouldn't happen"),
+        }
+    }
+}
+
+impl Widget for EmbedWidget {
+    fn render(
+        self,
+        area: ratatui::prelude::Rect,
+        buf: &mut ratatui::prelude::Buffer,
+    ) where
+        Self: Sized,
+    {
+        if let Embed::Record(record) = self.embed {
+            RecordWidget::new(record, self.is_selected).render(area, buf);
+        } else {
+            let borders = Block::bordered()
+                .style(self.style)
+                .border_set(symbols::border::ROUNDED);
+            let inner_area = borders.inner(area);
+            borders.render(area, buf);
+            self.non_record_paragraph().render(inner_area, buf);
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod embed;
+mod embed_widget;
 mod list;
 mod logger;
 mod post;
@@ -18,7 +19,8 @@ use bsky_sdk::{
     BskyAgent,
 };
 use crossterm::event::{self, Event, KeyCode};
-use embed::{Embed, Record};
+use embed::Record;
+use embed_widget::EmbedWidget;
 use itertools::Itertools;
 use list::{List, ListContext, ListState};
 use logger::{LOGGER, LOGSTORE};
@@ -27,7 +29,7 @@ use post_widget::PostWidget;
 use ratatui::{
     layout::{Constraint, Layout},
     prelude::{CrosstermBackend, StatefulWidget},
-    style::{Color, Modifier, Style},
+    style::{Color, Style},
     symbols,
     text::{Line, Span},
     widgets::{Block, Paragraph, Widget},
@@ -796,82 +798,6 @@ impl Widget for &mut Feed {
             }),
         )
         .render(area, buf, &mut self.state);
-    }
-}
-
-struct EmbedWidget {
-    embed: Embed,
-    style: Style,
-    is_selected: bool,
-}
-
-impl EmbedWidget {
-    fn new(embed: Embed, is_selected: bool) -> EmbedWidget {
-        EmbedWidget {
-            embed,
-            style: if is_selected {
-                Style::default().bg(Color::Rgb(45, 50, 55))
-            } else {
-                Style::default()
-            },
-            is_selected,
-        }
-    }
-
-    fn non_record_paragraph(&self) -> Paragraph {
-        match &self.embed {
-            Embed::Images(images) => Paragraph::new(
-                images
-                    .iter()
-                    .map(|image| {
-                        Line::from(format!("[image, alt: {}]", image.alt))
-                    })
-                    .collect::<Vec<Line>>(),
-            ),
-
-            Embed::Video(video) => {
-                Paragraph::new(format!("[video, alt: {}]", video.alt))
-            }
-
-            Embed::External(external) => Paragraph::new(vec![
-                Line::from(external.title.clone())
-                    .style(Style::default().add_modifier(Modifier::BOLD)),
-                Line::from(external.description.clone()),
-                Line::from(external.url.clone())
-                    .style(Style::default().add_modifier(Modifier::UNDERLINED)),
-            ]),
-
-            Embed::Record(_) => panic!("Shouldn't happen"),
-        }
-    }
-
-    fn line_count(&self, width: u16) -> u16 {
-        if let Embed::Record(record) = &self.embed {
-            RecordWidget::new(record.clone(), false).line_count(width) as u16
-        } else {
-            self.non_record_paragraph().line_count(width - 2) as u16 + 2
-        }
-    }
-}
-
-impl Widget for EmbedWidget {
-    fn render(
-        self,
-        area: ratatui::prelude::Rect,
-        buf: &mut ratatui::prelude::Buffer,
-    ) where
-        Self: Sized,
-    {
-        if let Embed::Record(record) = self.embed {
-            RecordWidget::new(record, self.is_selected).render(area, buf);
-        } else {
-            let borders = Block::bordered()
-                .style(self.style)
-                .border_set(symbols::border::ROUNDED);
-            let inner_area = borders.inner(area);
-            borders.render(area, buf);
-            self.non_record_paragraph().render(inner_area, buf);
-        }
     }
 }
 
